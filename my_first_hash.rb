af88084545846cbfb8956d6731ad435b8134056a
Christopher Osborn <z68ozz@gmail.com>
2020-05-06 00:18:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash
+def my_hash = { "whale" => "blue", "bear" => "polar"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
